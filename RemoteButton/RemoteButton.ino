ae3a7df884ace86ac9da6f2e24fc29e548390011
Grant Le Roux <grant@geekhelp.com.au>
2018-11-27 10:54:01 +1100

    Initial commit

---
+++ RemoteButton/RemoteButton.ino
@@ -0,0 +1,23 @@
+/*
+ Name:		RemoteButton.ino
+ Created:	11/25/2018 11:21:53 PM
+ Author:	grant
+*/
+
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include <WebServer.h>
+#include <DNSServer.h>
+#include <WiFi.h>
+#include "WILCO.h"
+
+WILCO wilco;
+
+void setup() {
+	Serial.begin(115200);
+	wilco.begin();
+}
+
+void loop() {
+	wilco.loop();
+}
